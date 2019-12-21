8cbcdbfc3b720e19391370c5b0a72f0b1b66064f
MAsadShaikh <57404492+MAsadShaikh@users.noreply.github.com>
2019-12-21 19:18:14 +0500

    Add files via upload

---
+++ assignment no 6.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Q1
+In object-oriented programming you write classes that represent real-world things and situations, and you create objects based on these classes.
+#Q2
+It provides a clear modular structure for programs which makes it good for defining abstract datatypes in which implementation details are hidden.
+#Q3
+function describes the behaviour of the model, they donot have any varable. methods are called by variables
+#Q4
+class is a function of attribues and functions.
+object is inheritance of a class 
+attributes are the variables your class have like model name etc 
+behaviour is the way the object can do anything
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
